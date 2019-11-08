007b57a9e53c8a0741bd7c102cb2cb7a2aa5b2c8
Fernando Luckesi <fernando.luckesi94@gmail.com>
2019-11-08 18:43:46 -0300

    versao02

--- fernandoluckesi/projeto1.sh
+++ fernandoluckesi/projeto1.sh
@@ -6,14 +6,15 @@
 API_KEY=6dc812e2b1193752da65422abf82478b
 CIDADE=$1
 RESPONSE=`curl -s -G --data-urlencode "appid=$API_KEY" --data-urlencode "q=$CIDADE" http://api.openweathermap.org/data/2.5/weather`
-echo $RESPONSE > texto.txt
-#cat texto.txt
-                       #"temp":[\d]*
-                      #"cod":"[\d]* 
+TEMPERATURA=$(echo $RESPONSE | jq '.main.temp')
+ERRO_MSG=$(echo $RESPONSE | jq '.cod')
+
 
 
 if [ -z "$1" ];then
     echo "Ops, você precisa passar o nome de uma cidade como argumento!"
     exit 1
+elif [[ "$ERRO_MSG" == "\"404\"" ]];then
+    echo "Ops, não encontrei a temperatura para esta cidade!"   
 fi
 
