27aa5055ad08bad1c1e38186c9739ecbd93c5753
Lucas Werner <lucas.1werner@gmail.com>
2019-11-08 19:41:33 -0300

    Temperatura da cidade (parcial)

--- lucas-werner/temperature.sh
+++ lucas-werner/temperature.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+echo "Digite o nome de uma cidade para saber a temperatura atual: "
+read CIDADE
 API_KEY=434d61d332b71d8b17a8bf7246f78d7e
-#RESPONSE=$(curl -s "http://api.openweathermap.org/data/2.5/weather?q=$1&units=metric&appid=$API_KEY")
-RESPONSE=$(curl -s -G --data-urlencode "appid=$API_KEY" --data-urlencode "q=$*" --data-urlencode "units=metric" http://api.openweathermap.org/data/2.5/weather)
-echo $RESPONSE
+
+if [ -z "$CIDADE" ]
+then
+    echo "Por gentileza, digite o nome de uma cidade!"
+    exit 1 #acaba o programa
+else
+
+RESPONSE=$(curl -s -G --data-urlencode "appid=$API_KEY" --data-urlencode "q=$CIDADE" --data-urlencode "units=metric" http://api.openweathermap.org/data/2.5/weather)
+echo $RESPONSE #regex para tratar a response
+
+fi
