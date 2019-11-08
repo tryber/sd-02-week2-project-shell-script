c6a91b8cc376888305768619af73d1be1b5d90f7
phtf92 <phtf92@gmail.com>
2019-11-08 19:09:36 -0200

    Programa finalizado sem IU

--- pedro-tofani/showTemp.sh
+++ pedro-tofani/showTemp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#capturando variável $CIDADE
-CIDADE=$(zenity --entry --title="Showtemp " --width=350 --height=150 --text="Qual sua cidade?") 
+#capturando var iável $CIDADE
+CIDADE=$(zenity --entry --title="ShowTemp" --width=350 --height=150 --text="Qual sua cidade?")
 
 #retornando: cidade não inserida
 if [[ -z "$CIDADE" ]]; then 
@@ -10,9 +10,11 @@ if [[ -z "$CIDADE" ]]; then
 else 
     #buscando cidade na API
     API_KEY=aeffd8cd63ab104d34dbce1049722f15
-    RESPONSE=`curl -s -G --data-urlencode "appid=$API_KEY" --data-urlencode "q=$CIDADE" http://api.openweathermap.org/data/2.5/weather`
+    RESPONSE=`curl -s -G --data-urlencode "appid=$API_KEY" --data-urlencode "q=$CIDADE" http://api.openweathermap.org/data/2.5/weather | jq '.main["temp"]'`
     echo $RESPONSE
+    
+    TEMPERATURA=`echo "scale=2; ($RESPONSE-273.15)" | bc`
 fi
 
 #retornando a cidade e a temperatura ao usuário
-#echo "A tempetura atual em $CIDADE é de $TEMPERATURA° Celsius!"
+zenity --info --title="ShowTemp" --width=350 --height=150 --text="Sua temperatura é $TEMPERATURA°C"
