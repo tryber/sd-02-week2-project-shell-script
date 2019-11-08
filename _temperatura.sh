50c7a96e165ff9340558ef752844a0caafca3795
EddyeBoy27 <lipe0503@gmail.com>
2019-11-08 17:32:42 -0200

    Criado script

---
+++ _temperatura.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+API_KEY=623af31f846719c17b41e27a8865eb1c
+
+echo "Digite o nome da cidade:"
+read CIDADE
+
+RESPONSE=`curl -s -G --data-urlencode "appid=$API_KEY" --data-urlencode "q=$CIDADE" http://api.openweathermap.org/data/2.5/weather`
+echo $RESPONSE
