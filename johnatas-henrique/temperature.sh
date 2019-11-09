e5652e4d1f4e3a374ae442f32e839cb227df5bb9
Johnatas Henrique <johnatas.henrique@gmail.com>
2019-11-09 16:05:14 -0300

    Vaquinha do shell e função país

--- johnatas-henrique/temperature.sh
+++ johnatas-henrique/temperature.sh
@@ -7,24 +7,25 @@ TMP_FILE=tmp_file.txt
 ping -w 1 8.8.8.8 >/dev/null 2>&1 #sumindo com a resposta do PING - Johnatas
 
 if [ $? != 0 ]; then #como teste, é válido esse tipo de PING, porém caso acontecesse uma falha nos servers de DNS do Google, o script retornaria sem Internet erroneamente.
-    echo "Ops, parece que você está sem internet! Ou o site que o script envia pacotes ICMP está fora do ar."
+    xcowsay --at=200,250 $'Ops, parece que você está sem internet!\nOu o site que o script envia o PING para teste está fora do ar.'
 else 
 
     if [ -z "$CIDADE" ]; then
-        echo "Ops, você precisa passar o nome de uma cidade como argumento!"
+        xcowsay --at=200,250 $'Ops, você precisa passar o nome de uma cidade como argumento!\nVou fechar aqui, então chame o script novamente e digite a cidade ok?'
     else
 
         RESPONSE=$(curl -s -G --data-urlencode "appid=$API_KEY" --data-urlencode "q=$CIDADE" --data-urlencode "units=metric" http://api.openweathermap.org/data/2.5/weather)
-        echo $RESPONSE
+        #echo $RESPONSE #Deixei aqui para testes rápidos, caso necessário - Johnatas
         echo $RESPONSE > $TMP_FILE
         NOT_FOUND_ERROR=$(grep -o -E "\"cod\":\"404\"" $TMP_FILE)
 
         if [ $NOT_FOUND_ERROR == '"cod":"404"' ] 2> /dev/null; then  #resolvendo mensagem de falha de operador unário - Johnatas
-            echo "Ops, não encontrei a temperatura para esta cidade! A grafia está correta?"
+            xcowsay --at=400,250 $'Ops, não encontrei a temperatura para esta cidade! A grafia está correta?'
         else
             CIDADE_TEMP=$(grep -o -E '"temp":[^,]+' $TMP_FILE | grep -o -E "[^:]+$")
             TEMPERATURA_TR=$(echo $CIDADE_TEMP | tr . ,) #tr no ., porque no Brasil utilizamos ',' como separador de decimais - Johnatas
-            echo "Em $CIDADE, a temperatura é de $TEMPERATURA_TRºC."
+            PAIS=$(grep -o -E '"country":[^,]+' $TMP_FILE | grep -o -E "[A-Z]{2}") #Colocar o país por conta de que algumas cidades tem nomes iguais (Santos, por exemplo) - Johnatas
+            xcowsay --at=400,250 $"Em $CIDADE, $PAIS, a temperatura é de $TEMPERATURA_TRºC."
         fi
         rm tmp_file.txt #para apagar o arquivo temporário após utilizar o script - Johnatas
     fi
