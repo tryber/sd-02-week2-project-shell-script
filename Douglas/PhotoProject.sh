7e1dd173d643672ed50d04e792d694fdc67c77f1
Douglas Henrique <doughrqe@gmail.com>
2019-11-09 16:02:40 -0300

    layout mais interativo

--- Douglas/PhotoProject.sh
+++ Douglas/PhotoProject.sh
@@ -1,48 +1,52 @@
 #!/bin/bash
-#inserir interface melhor
+#inserir interface
 API_KEY=14207965-9493642060cb1c95148269738
-echo "Imagem de que você procura?"
-read SEARCH_TERM
-echo "Quantas imagens de $SEARCH_TERM você quer?"
-read QTY_RESULTS
-if [ $SEARCH_TERM -z ] > /dev/null 2> /dev/null
-then
-    echo "Você deve passar um argumento valido!"
+xcowsay "Serei sua assistente, qual imagem você procura?" > /dev/null 2> /dev/null
+Busca=$(yad --entry --height="100" --width="200" --title="Responda a vaquinha" --buttons-layout=center --button="gtk-ok:0" )
+if [ $? -eq 252 ]
+then 
+    xcowsay "Até mais" > /dev/null 2> /dev/null
     exit 1
-elif [ $QTY_RESULTS -z ] > /dev/null 2> /dev/null
+elif [ $Busca -z ] > /dev/null 2> /dev/null
 then
-    QTY_RESULTS=3
+    xcowsay "Sem imagem, sem download, até mais"  > /dev/null 2> /dev/null
+    exit 1
 fi
-if (($QTY_RESULTS < 3 || $QTY_RESULTS > 200))
+xcowsay "Quantas imagens de $Busca você quer?"  > /dev/null 2> /dev/null
+Q_Fotos=$(yad --entry --height="100" --width="200" --title="Responda a vaquinha" --buttons-layout=center --button="gtk-ok:0" )
+if [ $? -eq 252 ]
+then 
+    xcow say "Até mais" > /dev/null 2> /dev/null
+    exit 1
+fi
+if [ $Q_Fotos -z ] > /dev/null 2> /dev/null
 then
-    echo "Você não passou um valor valido (de 3 a 200)"
+    Q_Fotos=3
+fi
+if (( $Q_Fotos < 3 ))
+then
+    xcowsay "Desculpa, so consigo pegar o minimo de 3 imagens :C" > /dev/null 2> /dev/null
+    exit 1
+elif (( $Q_Fotos > 200 ))
+then
+    xcowsay "Desculpa, so consigo pegar 200 imagens por pesquisa :C" > /dev/null 2> /dev/null
     exit 1
 fi
 
-RESPONSE=`curl -s -G -L --data-urlencode "key=$API_KEY" --data-urlencode "q=$SEARCH_TERM" --data-urlencode "image_type=photo" --data-urlencode "per_page=$QTY_RESULTS" https://pixabay.com/api/`;
+RESPONSE=`curl -s -G -L --data-urlencode "key=$API_KEY" --data-urlencode "q=$Busca" --data-urlencode "image_type=photo" --data-urlencode "per_page=$Q_Fotos" https://pixabay.com/api/`;
 echo $RESPONSE > teste.txt
 
 encontrado=`cut -d ":" -f2 teste.txt | cut -d ',' -f1`
 
 if [ $encontrado -eq 0 ]
 then
-    echo "Que pena, nenhuma imagem de $SEARCH_TERM foi encontrada"
+    xcowsay "Que pena, nenhuma imagem de $Busca foi encontrada, Até mais" > /dev/null 2> /dev/null
     exit 0
 fi
-if [ $SEARCH_TERM -e ] > /dev/null 2> /dev/null
-then
-    mv teste.txt ./$SEARCH_TERM
-    cd $SEARCH_TERM
+    mkdir $Busca  > /dev/null 2> /dev/null
+    mv teste.txt ./$Busca  
+    cd $Busca
     termo=`egrep -o 'webformatURL":"https://pixabay.com/get/(\d+|\w+)+.\w{3}' teste.txt | cut -d '"' -f3`
     wget --quiet $termo 
     rm teste.txt
-    echo "Suas imagens ja estão na pasta $SEARCH_TERM"
-else
-    mkdir $SEARCH_TERM
-    mv teste.txt ./$SEARCH_TERM
-    cd $SEARCH_TERM
-    termo=`egrep -o 'webformatURL":"https://pixabay.com/get/(\d+|\w+)+.\w{3}' teste.txt | cut -d '"' -f3`
-    wget --quiet $termo 
-    rm teste.txt
-    echo "Suas imagens ja estão na pasta $SEARCH_TERM"
-fi
+    xcowsay "Suas imagens ja estão na pasta $Busca, Até a proxíma" > /dev/null 2> /dev/null
