4ed7fb3b9a7e60e2a7fae8e9db64b49a795b1a18
guicgs <etohlif@protonmail.com>
2019-11-09 23:53:41 -0300

    Consegui o >= e <= finalmente

--- guicgs/downloadImagens.sh
+++ guicgs/downloadImagens.sh
@@ -15,16 +15,38 @@ read qtdeResultados
 
 if [[ -z "$qtdeResultados" ]]; then
     qtdePadrao=3
-    echo "Por padrão, serão baixadas $qtdePadrao imagens!"
+    echo -e "Por padrão, serão baixadas três imagens!\nSuas imagens estão sendo baixadas..."
     RESPONSE=`curl -s -G -L --data-urlencode "key=$API_Key" --data-urlencode "q=$termoBusca" --data-urlencode "image_type=photo" --data-urlencode "lang=pt" --data-urlencode "per_page=$qtdePadrao" https://pixabay.com/api`
     echo $RESPONSE | egrep -o '"webformatURL":[^,]+' | egrep -o '(http[s]?|[s]?ftp[s]?)(:\/\/)([^\s,]+)([^"]+)' > lista.txt
+    
     mkdir $termoBusca 
     mv lista.txt ./$termoBusca/
-    cd $termoBusca
+    cd $termoBusca 
     wget -i lista.txt
-    # echo "Suas fotos foram salvas na pasta "$termoBusca""
+    
+    echo "Suas fotos foram salvas na pasta \"$termoBusca\""
+
     exit 1
 fi
+if (( "$qtdeResultados" <= 3 || "$qtdeResultados" >= 200 )); then
+    echo "Ops, o número de imagens deve estar entre 3 e 200."
+    exit 1
+fi
+elif [[ "$qtdeResultados" -ge 3 -le 200 ]]; then
+    echo "Suas imagens estão sendo baixadas..."
+    RESPONSE=`curl -s -G -L --data-urlencode "key=$API_Key" --data-urlencode "q=$termoBusca" --data-urlencode "image_type=photo" --data-urlencode "lang=pt" --data-urlencode "per_page=$qtdeResultados" https://pixabay.com/api`
+    echo $RESPONSE | egrep -o '"webformatURL":[^,]+' | egrep -o '(http[s]?|[s]?ftp[s]?)(:\/\/)([^\s,]+)([^"]+)' > lista.txt
+    
+    mkdir $termoBusca 
+    mv lista.txt ./$termoBusca/
+    cd $termoBusca 
+    wget -i lista.txt
+    
+    echo "Suas fotos foram salvas na pasta \"$termoBusca\""
 
-RESPONSE=`curl -s -G -L --data-urlencode "key=$API_Key" --data-urlencode "q=$termoBusca" --data-urlencode "image_type=photo" --data-urlencode "per_page=$qtdeResultados" https://pixabay.com/api`
-echo $RESPONSE
+else
+    echo "Ops, não encontrei nenhuma imagem para esta busca."
+fi
+
+#RESPONSE=`curl -s -G -L --data-urlencode "key=$API_Key" --data-urlencode "q=$termoBusca" --data-urlencode "image_type=photo" --data-urlencode "per_page=$qtdeResultados" https://pixabay.com/api`
+#echo $RESPONSE
