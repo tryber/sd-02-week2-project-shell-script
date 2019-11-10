a1b5da0d52547c73d6df5dcac6745db9e3ec1641
EddyeBoy27 <lipe0503@gmail.com>
2019-11-10 08:52:21 -0200

    Finalizado script

--- felipe/_temperatura.sh
+++ felipe/_temperatura.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
-
-API_KEY=623af31f846719c17b41e27a8865eb1c
-
+clear
+echo "Onde você deseja verificar o tempo hoje?"
 echo "Digite o nome da cidade:"
-read CIDADE
+    read -r CIDADE
 
-RESPONSE=`curl -s -G --data-urlencode "appid=$API_KEY" --data-urlencode "q=$CIDADE" http://api.openweathermap.org/data/2.5/weather`
-echo $RESPONSE | cat >> resultado.txt
+if [ -z "$CIDADE" ]
+    then
+        echo "Você precisa inserir o nome de uma cidade para continuar"
+        exit 1
+fi
+if [ ! -e "$CIDADE" ]
+    then
+        API_KEY=623af31f846719c17b41e27a8865eb1c
+        RESPONSE=`curl -s -G --data-urlencode "appid=$API_KEY" --data-urlencode "q=$CIDADE" http://api.openweathermap.org/data/2.5/weather`
+          echo  "$RESPONSE" | cat > resultado.txt
+                resulTemp=$(cat resultado.txt | grep -o -E '"main":{"temp":[^,]+' | grep -o -E "[^:]+$")
+                conversaoCelsius=`echo "scale=2; ($resulTemp-273.15)" | bc`
+    if [ $conversaoCelsius = -273.15 ]
+    then
+    echo "A cidade que você inseriu não existe, insira o nome corretamente"
+    exit
+    fi
+                echo "A temperatura em "$CIDADE" é de "$conversaoCelsius"º Celius"
+fi
+
+        
