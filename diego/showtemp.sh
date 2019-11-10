2ff6da9127d7a916257a403f20902fde246af624
Diego Augusto S F <contactdiegonow@gmail.com>
2019-11-09 22:05:26 -0300

    Diversas alterações na UI e minimização do código. Adicionado a verificação de conexão de internet.

--- diego/showtemp.sh
+++ diego/showtemp.sh
@@ -1,41 +1,63 @@
-#!/bin/bash
+    #!/bin/bash
+
+# instalação usando zenity
 
 # (
-# echo "10" ; sleep 1
-# echo "# Updating mail logs" ; sleep 1
-# echo "20" ; sleep 1
-# echo "# Resetting cron jobs" ; sleep 1
-# echo "50" ; sleep 1
-# echo "This line will just be ignored" ; sleep 1
-# echo "75" ; sleep 1
-# echo "# Rebooting system" ; sleep 1
-# echo "100" ; sleep 1
+# echo "0"; sleep 1
+# echo "# Verificando pacotes necessários" ; sleep 1
+# echo "10"; sleep 1;
+#     ZENITY_INSTALADO=$`dpkg -l | grep zenity    
+#     if ![[ -z  "$ZENITY_INSTALADO" ]]; then
+#         echo "Você não tem o Zenity instalado, para instalar execute: sudo apt install zenity"
+#     else
+#         echo "Você já tem o zenity instalado"`
+# echo "20"; sleep 1
+# echo "# Instalando o pacote $ZENITY_INSTALADO" 
+# echo "30"; sleep 1
+# echo "# Instalação em 20%" 
+# echo "40"; sleep 1
+# echo "# Instalação em 40%" 
+# echo "60"; sleep 1
+# echo "# Instalação em 60%" 
+# echo "65"; sleep 1
+# echo "# Instalação em 80%" 
+# echo "100"; sleep 1
+# echo "# ShowTemp instalado com sucesso" 
 # ) |
-# zenity --progress \
-#   --title="Update System Logs" \
-#   --text="Scanning mail logs..." \
+# zenity --progress --width=600 --height=100 \
+#   --title="ShowTemp Setup" \
+#   --text="Instalando ShowTemp" \
 #   --percentage=0
 
-# if [ "$?" = -1 ] ; then
-#         zenity --error \
-#           --text="Update canceled."
-# fi
+# # if [ "$?" = -1 ] ; then
+# #         zenity --error \
+# #           --text="Update canceled."
+# # fi
 
-#capturando var iável $CIDADE
-CIDADE=$(zenity --entry --title="ShowTemp" --width=350 --height=150 --text="Qual sua cidade?")
 
-#retornando: cidade não inserida
+# Verificar o acesso à internet:
+ping -w1 www.google.com.br >/dev/null 2>&1
+if [ $? != 0 ]; then
+    zenity --error --width=350 --height=150 --text="Você está sem conexão a internet :("
+    sleep 2s
+    ping -w1 www.google.com.br >/dev/null 2>&1
+else
+    #capturando variável $CIDADE
+    echo "internet ok"
+    CIDADE=$(zenity --entry --title="ShowTemp" --width=350 --height=150 --text="Qual sua cidade?")  
+fi
+
 if [[ -z "$CIDADE" ]]; then 
-    echo "Você não informou sua cidade :("
-    exit 1
+    #retornando erro ao não digitar cidade
+    zenity --warning  --title="ShowTemp" --width=350 --height=150 --text="\n\n                         Você não informou sua cidade :("
+    exit 1 
 else 
     #buscando cidade na API
     API_KEY=aeffd8cd63ab104d34dbce1049722f15
     RESPONSE=`curl -s -G --data-urlencode "appid=$API_KEY" --data-urlencode "q=$CIDADE" http://api.openweathermap.org/data/2.5/weather | jq '.main["temp"]'`
     echo $RESPONSE
-    
     TEMPERATURA=`echo "scale=2; ($RESPONSE-273.15)" | bc`
 fi
 
 #retornando a cidade e a temperatura ao usuário
-zenity --info --title="ShowTemp" --width=350 --height=150 --text="Sua temperatura é $TEMPERATURA°C"
+yad --info --title="ShowTemp" --center --image=src/temperature.png --width=350 --height=150 --text="\n\nSua temperatura é $TEMPERATURA°C"
