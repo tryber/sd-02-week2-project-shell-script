1e20289a71d0027669cb6260e1d6ac2945929435
EddyeBoy27 <lipe0503@gmail.com>
2019-11-11 01:50:31 -0200

    Atualizado conforme Code Climate

--- felipe/_temperatura.sh
+++ felipe/_temperatura.sh
@@ -22,18 +22,16 @@ fi
 if [ ! -e "$CIDADE" ]
     then
         API_KEY=623af31f846719c17b41e27a8865eb1c
-        RESPONSE=`curl -s -G --data-urlencode "appid=$API_KEY" --data-urlencode "q=$CIDADE" http://api.openweathermap.org/data/2.5/weather`
+        RESPONSE=$(curl -s -G --data-urlencode "appid=$API_KEY" --data-urlencode "q=$CIDADE" http://api.openweathermap.org/data/2.5/weather)
           echo  "$RESPONSE" | cat > resultado.txt
-                resulTemp=$(cat resultado.txt | grep -o -E '"main":{"temp":[^,]+' | grep -o -E "[^:]+$")
-                conversaoCelsius=`echo "scale=2; ($resulTemp-273.15)" | bc`
-    if [ $conversaoCelsius = -273.15 ]
+                resulTemp=$(cat < resultado.txt | grep -o -E '"main":{"temp":[^,]+' | grep -o -E "[^:]+$")
+                conversaoCelsius=$(echo "scale=2; ($resulTemp-273.15)" | bc)
+    if [ "$conversaoCelsius" = -273.15 ]
     then
     echo "A cidade que você inseriu não existe, insira o nome corretamente"
     exit
     fi
-                echo "A temperatura em "$CIDADE" neste exato momento é de "$conversaoCelsius"º Celius"
+                echo "A temperatura em $CIDADE neste exato momento é de $conversaoCelsiusº Celius"
 fi
 
 rm -rf resultado.txt
-
-        
