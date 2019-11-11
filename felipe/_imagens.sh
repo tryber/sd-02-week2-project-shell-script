ab79a73e3410e00b64f825e9e9f1ba14ff529b62
EddyeBoy27 <lipe0503@gmail.com>
2019-11-11 01:38:02 -0200

    Bugs CodeClimate Fix

--- felipe/_imagens.sh
+++ felipe/_imagens.sh
@@ -1,9 +1,9 @@
-#!/bin/bash 
+#!/bin/bash
 clear
 API_Key=14225963-65f011eac0dcadc2019128ae2
 
 echo -e "Digite um termo de busca para download de imagens:\n(somente em português)"
-read termoBusca
+read -r termoBusca
 clear
 if [[ -z "$termoBusca" ]]; then
     echo "Ops, você precisa digitar um termo de busca!"
@@ -11,13 +11,13 @@ if [[ -z "$termoBusca" ]]; then
 fi
 clear
 echo "Quantas imagens você deseja baixar?"
-read qtdeResultados
+read -r qtdeResultados
 clear
 if [[ -z "$qtdeResultados" ]]; then
     qtdePadrao=3
     echo -e "Por padrão, serão baixadas três imagens!\nAguarde enquanto buscamos..."
-    RESPONSE=`curl -s -G -L --data-urlencode "key=$API_Key" --data-urlencode "q=$termoBusca" --data-urlencode "image_type=photo" --data-urlencode "lang=pt" --data-urlencode "per_page=$qtdePadrao" https://pixabay.com/api`
-    echo $RESPONSE | egrep -o '"webformatURL":[^,]+' | egrep -o '(http[s]?|[s]?ftp[s]?)(:\/\/)([^\s,]+)([^"]+)' > lista.txt
+    RESPONSE=$(curl -s -G -L --data-urlencode "key=$API_Key" --data-urlencode "q=$termoBusca" --data-urlencode "image_type=photo" --data-urlencode "lang=pt" --data-urlencode "per_page=$qtdePadrao" https://pixabay.com/api)
+    echo "$RESPONSE" | grep -o -E '"webformatURL":[^,]+' | grep -o -E '(http[s]?|[s]?ftp[s]?)(:\/\/)([^\s,]+)([^"]+)' > lista.txt
     if [[ ! -s lista.txt ]]; then
         echo "Ops, não encontrei nenhuma imagem para esta busca."
         rm -rf lista.txt
@@ -25,15 +25,15 @@ if [[ -z "$qtdeResultados" ]]; then
     fi
     clear
     echo "Suas imagens estão sendo baixadas!"
-    mkdir $termoBusca 
-    mv lista.txt ./$termoBusca/
-    cd $termoBusca 
+    mkdir "$termoBusca"
+    mv lista.txt ./"$termoBusca"/
+    cd "$termoBusca" || exit
     wget -i lista.txt -q
-    
+
     echo "Suas fotos foram salvas na pasta \"$termoBusca\""
     Menu(){
             echo -e "Você deseja compactar a pasta com os arquivos?\n[ 1 ] Sim\n[ 2 ] Não\n[ 3 ] Sair\nQual a opção desejada?"
-            read condicional
+            read -r condicional
 
             case $condicional in
                 1) Sim ;;
@@ -44,9 +44,9 @@ if [[ -z "$qtdeResultados" ]]; then
         }
         Sim () {
             cd ..
-            tar -zcf $termoBusca.tar.gz $termoBusca/
+            tar -zcf "$termoBusca".tar.gz "$termoBusca"/
             echo "Seu arquivo foi compactado"
-            rm -rf $termoBusca
+            rm -rf "$termoBusca"
             exit
         }
         Não () {
@@ -60,32 +60,32 @@ elif (( "$qtdeResultados" < 3 || "$qtdeResultados" > 200 )); then
     exit 1
 
 elif (( "$qtdeResultados" >= 3 || "$qtdeResultados" <= 200 )); then
-    
+
     echo "Aguarde enquanto buscamos..."
 
-    RESPONSE=`curl -s -G -L --data-urlencode "key=$API_Key" --data-urlencode "q=$termoBusca" --data-urlencode "image_type=photo" --data-urlencode "lang=pt" --data-urlencode "per_page=$qtdeResultados" https://pixabay.com/api`
-    
-    echo $RESPONSE | egrep -o '"webformatURL":[^,]+' | egrep -o '(http[s]?|[s]?ftp[s]?)(:\/\/)([^\s,]+)([^"]+)' > lista.txt
-    
+    RESPONSE=$(curl -s -G -L --data-urlencode "key=$API_Key" --data-urlencode "q=$termoBusca" --data-urlencode "image_type=photo" --data-urlencode "lang=pt" --data-urlencode "per_page=$qtdeResultados" https://pixabay.com/api)
+
+    echo "$RESPONSE" | grep -o -E '"webformatURL":[^,]+' | grep -o -E '(http[s]?|[s]?ftp[s]?)(:\/\/)([^\s,]+)([^"]+)' > lista.txt
+
     if [[ ! -s lista.txt ]]; then
         echo "Ops, não encontrei nenhuma imagem para esta busca."
         rm -rf lista.txt
         exit 1
     fi
-    
+
     echo "Suas imagens estão sendo baixadas..."
-    
-    mkdir $termoBusca 
-    mv lista.txt ./$termoBusca/
-    cd $termoBusca 
+
+    mkdir "$termoBusca"
+    mv lista.txt ./"$termoBusca"/
+    cd "$termoBusca" || exit
     wget -i lista.txt -q
-    rm -rf lista.txt 
+    rm -rf lista.txt
     clear
     echo "Suas fotos foram salvas na pasta \"$termoBusca\"!"
-        
+
         Menu(){
             echo -e "Você deseja compactar a pasta com os arquivos?\n[ 1 ] Sim\n[ 2 ] Não\n[ 3 ] Sair\nQual a opção desejada?"
-            read condicional
+            read -r condicional
 
             case $condicional in
                 1) Sim ;;
@@ -96,9 +96,9 @@ elif (( "$qtdeResultados" >= 3 || "$qtdeResultados" <= 200 )); then
         }
         Sim () {
             cd ..
-            tar -zcf $termoBusca.tar.gz $termoBusca/
+            tar -zcf "$termoBusca".tar.gz "$termoBusca"/
             echo "Seu arquivo foi compactado"
-            rm -rf $termoBusca
+            rm -rf "$termoBusca"
             exit
         }
         Não () {
