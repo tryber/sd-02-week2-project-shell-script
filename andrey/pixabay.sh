daa619ca95c4eed892774bea41202d9c4ff43bb1
Andrey <andreytsuzuki@gmail.com>
2019-11-14 13:51:21 -0300

    coisa

--- andrey/pixabay.sh
+++ andrey/pixabay.sh
@@ -1,60 +1,44 @@
 #!/bin/bash
-
 while [[ $SEARCH_TERM = "" ]]
 do
     read -p "Bem vindo à busca de imagens do Pixabay pelo terminal! O que você gostaria de ver? " SEARCH_TERM
-    if [[ -z "$SEARCH_TERM" ]]; then
-        echo "Desculpe, você precisa pesquisar um valor. O programa será encerrado." 
-        exit   
+    if [[ $SEARCH_TERM = "" ]]; then
+        echo "Desculpe, você não digitou um termo válido para ser pesquisado. O programa será encerrado."
+        exit 
     fi   
 done
-
-while [[ $(($QTY_RESULTS)) -le 2 || $(($QTY_RESULTS)) -gt 201 ]]
+while [[ $QTY_RESULTS = "" || $(($QTY_RESULTS)) -le 2 || $(($QTY_RESULTS)) -gt 201 ]]
 do
     read -p "Qual a quantidade que você deseja de imagens de $SEARCH_TERM? Escolha uma quantidade entre 3 e 200 fotos! " QTY_RESULTS
-    if [[ $(($QTY_RESULTS)) -gt 0 && $(($QTY_RESULTS)) -lt 2 || $(($QTY_RESULTS)) -gt 201 ]]; then
-        echo "Você deve digitar uma quantidade entre 3 e 200 para que possa funcionar."
-    elif [ -z "$QTY_RESULTS" ]; then
-        echo "Quantidade assumida por padrão: 3"
+    if [[ $QTY_RESULTS  = "" || $(($QTY_RESULTS)) -lt 2 || $(($QTY_RESULTS)) -gt 201 ]]; then
+        echo "Buscando a quantidade mínima de imagens que é 3."
         QTY_RESULTS=$((3))
         break
     fi
 done
-
 echo "Verificando se o diretório './foto' existe."
 if [ ! -d foto ];
 then
     echo "Criando o diretório 'foto'."
     mkdir foto
 fi
-
 echo "Entrando no diretório 'foto'."
 cd foto
-
 API_KEY=14214201-9d62060be38de7be7ac88482d
 RESPONSE=`curl -s -G -L --data-urlencode "key=$API_KEY" --data-urlencode "q=$SEARCH_TERM" --data-urlencode "image_type=photo" --data-urlencode "per_page=$QTY_RESULTS" https://pixabay.com/api`
-
 RESPONSE_INDEX=0
-
-API_KEY=14214201-9d62060be38de7be7ac88482d
-SEARCH_TERM="kidwqdnwns"
-QTY_RESULTS=3
-RESPONSE=`curl -s -G -L --data-urlencode "key=$API_KEY" --data-urlencode "q=$SEARCH_TERM" --data-urlencode "image_type=photo" --data-urlencode "per_page=$QTY_RESULTS" https://pixabay.com/api`
-echo $RESPONSE
-
+ERRO=`echo $RESPONSE | egrep -o "\"totalHits\":[0:9]*" | cut -s -d ":" -f2`
+echo $ERRO
+if [[ "$ERRO" == "0" ]]; then
+    echo "Ops! Não encontrei nada por aqui... tente outro termo!"
+    exit 1
+fi
 while [ $RESPONSE_INDEX -lt $QTY_RESULTS ]
 do
-  if [ $(echo $RESPONSE | jq .hits[0]) = null ]; then
-    echo "api não encontrou imagens sobre: $SEARCH_TERM"
-  break
-  else
     IMAGE_URL=$(echo $RESPONSE | jq .hits[$RESPONSE_INDEX].largeImageURL | sed 's/"//g')
     curl $IMAGE_URL > $RESPONSE_INDEX.jpg
-	RESPONSE_INDEX=$[ $RESPONSE_INDEX + 1 ]
-  fi
+	RESPONSE_INDEX=$[ $RESPONSE_INDEX + $SEARCH_TERM + 1 ]
 done
-
-
 while [[ $SHOULD_COMPACT != "S" && $SHOULD_COMPACT != "N" ]]
 do
     read -p "Deseja compactar seu pedido em um arquivo? (S/N)" SHOULD_COMPACT
@@ -62,7 +46,6 @@ do
     echo "Escolha uma opção entre S ou N."
     fi   
 done
-
 if [ $SHOULD_COMPACT = "S" ]
 then
 echo "Iniciando compactação."
